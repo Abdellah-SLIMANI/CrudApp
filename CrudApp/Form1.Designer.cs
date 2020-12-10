1206625a4b9570b71efacc8cec7e1a3a247baf4c
morscor21@gmail.com <morscor21@gmail.com>
2020-12-10 23:33:44 +0100

    Adding values to a table Impelemntation

--- CrudApp/Form1.Designer.cs
+++ CrudApp/Form1.Designer.cs
@@ -50,6 +50,7 @@ namespace CrudApp
             this.button1.TabIndex = 0;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- CrudApp/Form1.cs
+++ CrudApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CrudApp
 {
@@ -20,6 +21,18 @@ namespace CrudApp
         private void button2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
+            connecting.Open();
+            SqlCommand command = new SqlCommand("insert into [Table] values (@Username,@Email,@password)",connecting);
+            command.Parameters.AddWithValue("@Username", textBox1.Text);
+            command.Parameters.AddWithValue("@Email", textBox2.Text);
+            command.Parameters.AddWithValue("@password", textBox3.Text);
+            command.ExecuteNonQuery();
+            connecting.Close();
         }
     }
 }
