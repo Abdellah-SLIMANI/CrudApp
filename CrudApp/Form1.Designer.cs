0380265dafc015b2ae79c9e080bc5284c91a98b3
morscor21@gmail.com <morscor21@gmail.com>
2020-12-10 23:49:38 +0100

    Added a filter to the controls to Guide the user of what he will need to use (e.g:Delete Function)

--- CrudApp/Form1.Designer.cs
+++ CrudApp/Form1.Designer.cs
@@ -40,6 +40,7 @@ namespace CrudApp
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -79,6 +80,7 @@ namespace CrudApp
             this.button4.TabIndex = 3;
             this.button4.Text = "Delete";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // textBox1
             // 
@@ -141,11 +143,22 @@ namespace CrudApp
             this.label4.TabIndex = 10;
             this.label4.Text = "Accounts Managment";
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(212, 290);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Submit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(518, 325);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -177,6 +190,7 @@ namespace CrudApp
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- CrudApp/Form1.cs
+++ CrudApp/Form1.cs
@@ -18,21 +18,74 @@ namespace CrudApp
             InitializeComponent();
         }
 
+        string WhatDidIJustClicked = "";
+
+        public void initState()
+        {
+            if(WhatDidIJustClicked == "")
+            {
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
-
+            WhatDidIJustClicked = "button2";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
-            connecting.Open();
-            SqlCommand command = new SqlCommand("insert into [Table] values (@Username,@Email,@password)",connecting);
-            command.Parameters.AddWithValue("@Username", textBox1.Text);
-            command.Parameters.AddWithValue("@Email", textBox2.Text);
-            command.Parameters.AddWithValue("@password", textBox3.Text);
-            command.ExecuteNonQuery();
-            connecting.Close();
+            WhatDidIJustClicked = "button1";
         }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WhatDidIJustClicked = "button4";
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (WhatDidIJustClicked == "button4")
+            {
+                SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
+                connecting.Open();
+                SqlCommand command = new SqlCommand("Delete [Table] where username=@Username", connecting);
+                command.Parameters.AddWithValue("@Username", textBox1.Text);
+                command.ExecuteNonQuery();
+                connecting.Close();
+            }
+            WhatDidIJustClicked = "";
+            initState();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
+        //    connecting.Open();
+        //    SqlCommand command = new SqlCommand("insert into [Table] values (@Username,@Email,@password)",connecting);
+        //    command.Parameters.AddWithValue("@Username", textBox1.Text);
+        //    command.Parameters.AddWithValue("@Email", textBox2.Text);
+        //    command.Parameters.AddWithValue("@password", textBox3.Text);
+        //    command.ExecuteNonQuery();
+        //    connecting.Close();
+        //}
+
+        //private void button4_Click(object sender, EventArgs e)
+        //{
+        //    SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
+        //    connecting.Open();
+        //    SqlCommand command = new SqlCommand("Delete [Table] where username=@Username", connecting);
+        //    command.Parameters.AddWithValue("@Username", textBox1.Text);
+        //    command.ExecuteNonQuery();
+        //    connecting.Close();
+        //}
     }
 }
