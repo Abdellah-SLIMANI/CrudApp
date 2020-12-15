0a045609bf82403f0c6757d79d3279a33c2b7198
morscor21@gmail.com <morscor21@gmail.com>
2020-12-15 02:19:35 +0100

    cleaning up the whole codeBase

--- CrudApp/Form1.cs
+++ CrudApp/Form1.cs
@@ -17,13 +17,46 @@ namespace CrudApp
         {
             InitializeComponent();
         }
-
+        //this will keep us in track of the buttons we clicked (According to the state)
         string WhatDidIJustClick = "";
 
+        //These Controls will help on creating a popup on the Update phase 
+        //Bonus Controls
+        TextBox feild = new TextBox()
+        {
+            Location = new Point(10, 50),
+            TabIndex = 11
+        };
+
+        Button botona = new Button()
+        {
+            Text = "Submit",
+            Location = new Point(10, 100),
+        };
+
+        Label malabel = new Label()
+        {
+            Text = "Username",
+            Location = new Point(10, 10)
+        };
+        //End of Bonus Controls
+
+        //Queries ===> we will be using these instead of writing everything in there
+        string addQuery = "insert into [Table] values (@Username,@Email,@password)";
+        string updateQuery = "update[Table] set Email = @Email, password = @password where username = @Username";
+        string deleteQuery = "Delete [Table] where username=@Username";
+        //bonus (Data base name)
+        string DBName = @"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True";
+
+        //WE ARE GOING TO DISABLE USLESS CONTROLS WHEN WE CLICK A BUTTON
+
+        //This Funtions will Reset/Enable every Control so we can reuse everything
         public void initState()
         {
             if (WhatDidIJustClick == "")
             {
+                //enabling...
+                textBox1.Enabled = true;
                 textBox2.Enabled = true;
                 textBox3.Enabled = true;
                 button1.Enabled = true;
@@ -32,11 +65,7 @@ namespace CrudApp
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            WhatDidIJustClick = "button2";
-        }
-
+        //disabling stuff when other stuff is clicked
         private void button1_Click(object sender, EventArgs e)
         {
             WhatDidIJustClick = "button1";
@@ -52,33 +81,46 @@ namespace CrudApp
             button3.Enabled = false;
         }
 
+        //this is the SUBMIT button and it takes on concideration the last button clicked (to know what to do)
         private void button5_Click(object sender, EventArgs e)
         {
-            SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
+            //Connecting to the DB
+            SqlConnection connecting = new SqlConnection(DBName);
             connecting.Open();
 
+            //Here we clciked on Delete so we are running a delete query 
             if (WhatDidIJustClick == "button4")
             {
-                SqlCommand command = new SqlCommand("Delete [Table] where username=@Username", connecting);
+                SqlCommand command = new SqlCommand(deleteQuery, connecting);
                 command.Parameters.AddWithValue("@Username", textBox1.Text);
                 command.ExecuteNonQuery();
 
-            } else if (WhatDidIJustClick == "button1")
+            } 
+            //running an insertion query because button1 (Add) was the last to be clicked
+            else if (WhatDidIJustClick == "button1")
             {
-                SqlCommand command = new SqlCommand("insert into [Table] values (@Username,@Email,@password)", connecting);
+                SqlCommand command = new SqlCommand(addQuery, connecting);
                 command.Parameters.AddWithValue("@Username", textBox1.Text);
                 command.Parameters.AddWithValue("@Email", textBox2.Text);
                 command.Parameters.AddWithValue("@password", textBox3.Text);
                 command.ExecuteNonQuery();
-            } else if (WhatDidIJustClick == "button3")
+            } 
+            //the update query (currently not working)
+            else if (WhatDidIJustClick == "button3")
             {
-
+                SqlCommand command = new SqlCommand(updateQuery, connecting);
+                command.Parameters.AddWithValue("@Username", feild.Text);
+                command.Parameters.AddWithValue("@Email", textBox2.Text);
+                command.Parameters.AddWithValue("@password", textBox3.Text);
+                command.ExecuteNonQuery();
             }
             connecting.Close();
+            //we are sitting everything to the inital state 
             WhatDidIJustClick = "";
             initState();
         }
 
+        //This one is for getting the data and shwoing it on the gridView
         private void button6_Click(object sender, EventArgs e)
         {
             SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
@@ -90,44 +132,33 @@ namespace CrudApp
             dataGridView1.DataSource = dataTab;
         }
 
+        //Abort Button to re-Intialize things
         private void button7_Click(object sender, EventArgs e)
         {
             WhatDidIJustClick = "";
             initState();
         }
 
+        //Here we are creating a form to specify what we want to update exactly (in this case username kind of the id)
         private void button3_Click(object sender, EventArgs e)
         {
+            //disabling
+            WhatDidIJustClick = "button3";
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            button4.Enabled = false;
+
             Form popUp = new Form();
 
-            Label label4 = new Label()
-            {
-                Text = "Username",
-                Location = new Point(10, 10)
-            };
-
-            TextBox feild = new TextBox()
-            {
-                Location = new Point(10, 50),
-                TabIndex = 11
-            };
-
-            Button botona = new Button()
-            {
-                Text = "Submit",
-                Location = new Point(10, 100),
-            };
-
+            //adding the stuff on the variables here
             popUp.Controls.Add(feild);
-            popUp.Controls.Add(label4);
+            popUp.Controls.Add(malabel);
             popUp.Controls.Add(botona);
 
             popUp.Show();
+            //extiing popUp
             botona.Click += botona_clicked;
-            void botona_clicked(object seander, EventArgs ea)
-            {
-                popUp.Close();
-            }
+            void botona_clicked(object botona_sender, EventArgs botona_e) => popUp.Close();
         }
 
 
