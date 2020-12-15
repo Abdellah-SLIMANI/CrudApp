87f512176f3c112cfe5394ab9dc255bcad23516d
morscor21@gmail.com <morscor21@gmail.com>
2020-12-15 01:44:30 +0100

    added datat visuals and Abort and update function

--- CrudApp/Form1.Designer.cs
+++ CrudApp/Form1.Designer.cs
@@ -30,7 +30,6 @@ namespace CrudApp
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -41,11 +40,15 @@ namespace CrudApp
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(64, 247);
+            this.button1.Location = new System.Drawing.Point(114, 247);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -53,28 +56,19 @@ namespace CrudApp
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(167, 247);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Read";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(269, 247);
+            this.button3.Location = new System.Drawing.Point(231, 247);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Update";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(367, 247);
+            this.button4.Location = new System.Drawing.Point(344, 247);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
@@ -145,7 +139,7 @@ namespace CrudApp
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(212, 290);
+            this.button5.Location = new System.Drawing.Point(167, 290);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 11;
@@ -153,11 +147,42 @@ namespace CrudApp
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(501, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(454, 243);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(694, 290);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Refresh";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(269, 290);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Abort";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 325);
+            this.ClientSize = new System.Drawing.Size(972, 331);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -168,10 +193,10 @@ namespace CrudApp
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,7 +205,6 @@ namespace CrudApp
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox1;
@@ -191,6 +215,9 @@ namespace CrudApp
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- CrudApp/Form1.cs
+++ CrudApp/Form1.cs
@@ -18,39 +18,37 @@ namespace CrudApp
             InitializeComponent();
         }
 
-        string WhatDidIJustClicked = "";
+        string WhatDidIJustClick = "";
 
         public void initState()
         {
-            if(WhatDidIJustClicked == "")
+            if (WhatDidIJustClick == "")
             {
                 textBox2.Enabled = true;
                 textBox3.Enabled = true;
                 button1.Enabled = true;
-                button2.Enabled = true;
                 button3.Enabled = true;
+                button4.Enabled = true;
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            WhatDidIJustClicked = "button2";
+            WhatDidIJustClick = "button2";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            WhatDidIJustClicked = "button1";
-            button2.Enabled = false;
+            WhatDidIJustClick = "button1";
             button3.Enabled = false;
             button4.Enabled = false;
         }
         private void button4_Click(object sender, EventArgs e)
         {
-            WhatDidIJustClicked = "button4";
+            WhatDidIJustClick = "button4";
             textBox2.Enabled = false;
             textBox3.Enabled = false;
             button1.Enabled = false;
-            button2.Enabled = false;
             button3.Enabled = false;
         }
 
@@ -59,45 +57,79 @@ namespace CrudApp
             SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
             connecting.Open();
 
-            if (WhatDidIJustClicked == "button4")
+            if (WhatDidIJustClick == "button4")
             {
                 SqlCommand command = new SqlCommand("Delete [Table] where username=@Username", connecting);
                 command.Parameters.AddWithValue("@Username", textBox1.Text);
                 command.ExecuteNonQuery();
 
-            } else if(WhatDidIJustClicked == "button1")
+            } else if (WhatDidIJustClick == "button1")
             {
                 SqlCommand command = new SqlCommand("insert into [Table] values (@Username,@Email,@password)", connecting);
                 command.Parameters.AddWithValue("@Username", textBox1.Text);
                 command.Parameters.AddWithValue("@Email", textBox2.Text);
                 command.Parameters.AddWithValue("@password", textBox3.Text);
                 command.ExecuteNonQuery();
+            } else if (WhatDidIJustClick == "button3")
+            {
+
             }
             connecting.Close();
-            WhatDidIJustClicked = "";
+            WhatDidIJustClick = "";
             initState();
         }
 
-        //private void button1_Click(object sender, EventArgs e)
-        //{
-        //    SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
-        //    connecting.Open();
-        //    SqlCommand command = new SqlCommand("insert into [Table] values (@Username,@Email,@password)",connecting);
-        //    command.Parameters.AddWithValue("@Username", textBox1.Text);
-        //    command.Parameters.AddWithValue("@Email", textBox2.Text);
-        //    command.Parameters.AddWithValue("@password", textBox3.Text);
-        //    command.ExecuteNonQuery();
-        //    connecting.Close();
-        //}
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
+            connecting.Open();
+            SqlCommand command = new SqlCommand("Select * from [Table]", connecting);
+            SqlDataAdapter data = new SqlDataAdapter(command);
+            DataTable dataTab = new DataTable();
+            data.Fill(dataTab);
+            dataGridView1.DataSource = dataTab;
+        }
 
-        //private void button4_Click(object sender, EventArgs e)
-        //{
-        //    SqlConnection connecting = new SqlConnection(@"Data Source=DESKTOP-QJQOIK3\MSSQLSERVER01;Initial Catalog=CrudApp;Integrated Security=True");
-        //    connecting.Open();
-        //    SqlCommand command = new SqlCommand("Delete [Table] where username=@Username", connecting);
-        //    command.Parameters.AddWithValue("@Username", textBox1.Text);
-        //    command.ExecuteNonQuery();
-        //    connecting.Close();
-        //}
+        private void button7_Click(object sender, EventArgs e)
+        {
+            WhatDidIJustClick = "";
+            initState();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form popUp = new Form();
+
+            Label label4 = new Label()
+            {
+                Text = "Username",
+                Location = new Point(10, 10)
+            };
+
+            TextBox feild = new TextBox()
+            {
+                Location = new Point(10, 50),
+                TabIndex = 11
+            };
+
+            Button botona = new Button()
+            {
+                Text = "Submit",
+                Location = new Point(10, 100),
+            };
+
+            popUp.Controls.Add(feild);
+            popUp.Controls.Add(label4);
+            popUp.Controls.Add(botona);
+
+            popUp.Show();
+            botona.Click += botona_clicked;
+            void botona_clicked(object seander, EventArgs ea)
+            {
+                popUp.Close();
+            }
+        }
+
+
     }
 }
